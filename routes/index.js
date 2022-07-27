93c7636165dd399eb1f6be38861c8ac218af24e6
Mateus Mauricio Toledo <mateusmtoledo1@gmail.com>
2022-07-26 22:58:01 -0300

    Add messages feature

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  }
+];
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board', messages });
+});
+
+router.post('/new', (req,res) => {
+  const { text, user } = req.body;
+  messages.push({
+    text: text,
+    user: user,
+    added: new Date(),
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
